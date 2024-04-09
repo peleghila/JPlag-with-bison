4e33e590ad10c5782ccdeba1664151ca768db929
Alex | Kronox <alexander_vogt@outlook.de>
2024-04-09 12:19:16 +0200

    check click on line

--- report-viewer/tests/unit/components/fileDisplaying/CodePanel.test.ts
+++ report-viewer/tests/unit/components/fileDisplaying/CodePanel.test.ts
@@ -22,14 +22,16 @@ const match = {
   }
 }
 
+const props = {
+  file: file,
+  matches: [match],
+  highlightLanguage: ParserLanguage.TYPESCRIPT
+}
+
 describe('CodePanel', () => {
   it('Render code', async () => {
     const wrapper = mount(CodePanel, {
-      props: {
-        file: file,
-        matches: [],
-        highlightLanguage: ParserLanguage.TYPESCRIPT
-      }
+      props: props
     })
     await flushPromises()
 
@@ -41,11 +43,7 @@ describe('CodePanel', () => {
 
   it('Render file name', async () => {
     const wrapper = mount(CodePanel, {
-      props: {
-        file: file,
-        matches: [],
-        highlightLanguage: ParserLanguage.TYPESCRIPT
-      }
+      props: props
     })
     await flushPromises()
 
@@ -54,11 +52,7 @@ describe('CodePanel', () => {
 
   it('Render matches', async () => {
     const wrapper = mount(CodePanel, {
-      props: {
-        file: file,
-        matches: [match],
-        highlightLanguage: ParserLanguage.TYPESCRIPT
-      }
+      props: props
     })
     await flushPromises()
 
@@ -72,11 +66,7 @@ describe('CodePanel', () => {
 
   it('Highlight code', async () => {
     const wrapper = mount(CodePanel, {
-      props: {
-        file: file,
-        matches: [],
-        highlightLanguage: ParserLanguage.TYPESCRIPT
-      }
+      props: props
     })
     await flushPromises()
 
@@ -86,11 +76,7 @@ describe('CodePanel', () => {
 
   it('Show token count', async () => {
     const wrapper = mount(CodePanel, {
-      props: {
-        file: file,
-        matches: [],
-        highlightLanguage: ParserLanguage.TYPESCRIPT
-      }
+      props: props
     })
     await flushPromises()
 
@@ -101,11 +87,7 @@ describe('CodePanel', () => {
 
   it('Expand code', async () => {
     const wrapper = mount(CodePanel, {
-      props: {
-        file: file,
-        matches: [],
-        highlightLanguage: ParserLanguage.TYPESCRIPT
-      }
+      props: props
     })
     await flushPromises()
 
@@ -118,6 +100,49 @@ describe('CodePanel', () => {
     await clickable.trigger('click')
     await flushPromises()
     expect(getTableVisibility(wrapper)).toBe(false)
+  })
+
+  it('Emit line click', async () => {
+    const wrapper = mount(CodePanel, {
+      props: props
+    })
+    await flushPromises()
+
+    const text = wrapper.findAll('tr')[1]
+    text.trigger('click')
+    await flushPromises()
+
+    const emit = wrapper.emitted()['lineSelected']
+    expect(emit).toBeDefined()
+    expect(emit.length).toBe(1)
+  })
+
+  it('Collapse table on function call', async () => {
+    const wrapper = mount(CodePanel, {
+      props: props
+    })
+    await flushPromises()
+
+    const clickable = wrapper.getComponent(ToolTipComponent)
+
+    expect(getTableVisibility(wrapper)).toBe(false)
+    await clickable.trigger('click')
+    await flushPromises()
+    expect(getTableVisibility(wrapper)).toBe(true)
+    wrapper.vm.collapse()
+    await flushPromises()
+    expect(getTableVisibility(wrapper)).toBe(false)
+  })
+
+  // Disabled cause jsdom can at the moment not handle scrolling
+  it.skip('Show table on scroll to', async () => {
+    const wrapper = mount(CodePanel, {
+      props: props
+    })
+    await flushPromises()
+
+    wrapper.vm.scrollTo(2)
+    expect(getTableVisibility(wrapper)).toBe(true)
   })
 })
 
