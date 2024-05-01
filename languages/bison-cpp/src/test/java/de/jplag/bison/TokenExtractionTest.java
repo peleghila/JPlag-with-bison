ee1506e644977a1f140c1aa225448c000caf3383
peleghila <peleg.hila@gmail.com>
2024-05-01 09:26:56 +0300

    backing up some tests

--- languages/bison-cpp/src/test/java/de/jplag/bison/TokenExtractionTest.java
+++ languages/bison-cpp/src/test/java/de/jplag/bison/TokenExtractionTest.java
@@ -4,6 +4,7 @@ import de.jplag.ParsingException;
 import de.jplag.Token;
 import de.jplag.bison.grammar.BisonLexer;
 import de.jplag.bison.grammar.BisonParser;
+import de.jplag.bison.grammar.CPP14Lexer;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 
@@ -55,29 +56,56 @@ public class TokenExtractionTest {
             "            {\n" +
             "                cout << \"*** STOP RUN ***\" << endl;\n" +
             "                cout << driver.str() << endl;\n" +
-            "            }\n" +
+            "            };\n" +
             "        ;";
     @Test
     void testParseBison() throws IOException, ParsingException {
-        de.jplag.text.ParserAdapter textParser = new de.jplag.text.ParserAdapter();
+        //de.jplag.text.ParserAdapter textParser = new de.jplag.text.ParserAdapter();
         BisonParserAdapter parser = new BisonParserAdapter();
 
         File filePath = File.createTempFile("content","ypp");
         Files.writeString(filePath.toPath(), simple_bison);
-        List<Token> textTokensForPositions = textParser.parse(Set.of(filePath));
+        //List<Token> textTokensForPositions = textParser.parse(Set.of(filePath));
         List<Token> tokens = parser.scan(Set.of(filePath));
         System.out.println(tokens);
     }
 
     @Test
     void compareBisonToText() throws Exception {
-        de.jplag.text.ParserAdapter textParser = new de.jplag.text.ParserAdapter();
+        //de.jplag.text.ParserAdapter textParser = new de.jplag.text.ParserAdapter();
         BisonParserAdapter parser = new BisonParserAdapter();
 
         File filePath = new File("parser.ypp");
-        List<Token> textTokensForPositions = textParser.parse(Set.of(filePath));
+        //List<Token> textTokensForPositions = textParser.parse(Set.of(filePath));
         List<Token> tokens = parser.scan(Set.of(filePath));
 
         System.out.println(tokens);
     }
+
+    @Test
+    void testRawString() throws Exception {
+        CPP14Lexer lexer = new CPP14Lexer(CharStreams.fromString("R\"\"\"(((\n" +
+                "declare i32 @printf(i8*, ...)\n" +
+                "declare void @exit(i32)\n" +
+                "@.int_specifier = constant [4 x i8] c\"%d\\0A\\00\"\n" +
+                "@.str_specifier = constant [4 x i8] c\"%s\\0A\\00\"\n" +
+                "\n" +
+                "define void @printi(i32) {\n" +
+                "    %spec_ptr = getelementptr [4 x i8], [4 x i8]* @.int_specifier, i32 0, i32 0\n" +
+                "    call i32 (i8*, ...) @printf(i8* %spec_ptr, i32 %0)\n" +
+                "    ret void\n" +
+                "}\n" +
+                "\n" +
+                "define void @print(i8*) {\n" +
+                "    %spec_ptr = getelementptr [4 x i8], [4 x i8]* @.str_specifier, i32 0, i32 0\n" +
+                "    call i32 (i8*, ...) @printf(i8* %spec_ptr, i8* %0)\n" +
+                "    ret void\n" +
+                "}\n" +
+                ")))\"\"\"" +
+                "R\"(x = \"\"\\y\"\")\";"));
+        CommonTokenStream src = new CommonTokenStream(lexer);
+        src.fill();
+        System.out.println(src.getTokens());
+    }
+
 }
